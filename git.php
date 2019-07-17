012b17adaf322a88d3da37fae475d5e215e308c9
AjayYadavGBI <ajay_yadav@gbinternational.in>
2019-07-17 11:52:03 +0530

    adding extra new file

---
+++ git.php
@@ -0,0 +1,24 @@
+// there are some git commands which need to be revised everyday......
+
+git init
+git status
+git pull origin master
+git push origin master
+git branch
+git checkout branch_name
+git branch new_branch
+// merge branch
+
+git branch --merged // locally
+git merge my_branch
+git push origin master
+
+
+//Deete Branch
+
+git branch --merged
+git branch -d my_branch //Locally
+git push origin --delete my_branch // Remote location
+
+echo "content inside the new file" > fileName.txt
+
